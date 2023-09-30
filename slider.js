baa250ba8f0be30ec49c46b1949c6e897f397414
037wolf <131520924+037wolf@users.noreply.github.com>
2023-10-01 00:56:35 +0300

    .
    .

---
+++ slider.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
